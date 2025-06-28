a792c4374359a5fa1781cf2f7549afe1c4da7879
aborruso <aborruso@gmail.com>
2025-06-28 17:32:01 +0200

    aggiunge l'argomento '--no-sandbox' al lancio di Playwright nel script get_credentials.js

--- scripts/get_credentials.js
+++ scripts/get_credentials.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 const fs = require('fs');
 
 (async () => {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   // Create a promise that resolves when the credentials are found
