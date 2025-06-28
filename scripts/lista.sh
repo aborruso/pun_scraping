28aa41da0fb3164879cd9e60be0d245faa35e6fa
aborruso <aborruso@gmail.com>
2025-06-28 17:03:46 +0200

    corregge il percorso di output per i file JSONL e CSV nel script lista.sh

--- scripts/lista.sh
+++ scripts/lista.sh
@@ -9,6 +9,8 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 data=$(stat -c %y "${folder}"/../pun_pdr.ndjson | cut -d' ' -f1)
 
-jq -c '{status: .status, latitude: .coordinates.latitude, longitude: .coordinates.longitude, evse_id: .evse_id}' "${folder}"/../pun_pdr.ndjson > "${folder}"/../data/"${data}"_pun_pdr.jsonl
+jq -c '{status: .status, latitude: .coordinates.latitude, longitude: .coordinates.longitude, evse_id: .evse_id}' "${folder}"/../pun_pdr.ndjson > "${folder}"/../data/pun_pdr.jsonl
 
-mlr --ijsonl --ocsv cat "${folder}"/../data/"${data}"_pun_pdr.jsonl > "${folder}"/../data/"${data}"_pun_pdr.csv
+mlr -I --json sort -t evse_id "${folder}"/../data/pun_pdr.jsonl
+
+mlr --ijsonl --ocsv cat "${folder}"/../data/pun_pdr.jsonl > "${folder}"/../data/pun_pdr.csv
