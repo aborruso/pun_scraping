e40e57eda808fb29c7ee2c775013b939dc12dd45
aborruso <aborruso@gmail.com>
2025-06-29 09:16:07 +0200

    corregge il percorso del file pun_pdr.ndjson nel file lista.sh

--- scripts/lista.sh
+++ scripts/lista.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-jq -c '{status: .status, latitude: .coordinates.latitude, longitude: .coordinates.longitude, evse_id: .evse_id}' "${folder}"/../pun_pdr.ndjson > "${folder}"/../data/pun_pdr.jsonl
+jq -c '{status: .status, latitude: .coordinates.latitude, longitude: .coordinates.longitude, evse_id: .evse_id}' "${folder}"/../data/pun_pdr.ndjson > "${folder}"/../data/pun_pdr.jsonl
 
 mlr -I --jsonl sort -t evse_id "${folder}"/../data/pun_pdr.jsonl
 
